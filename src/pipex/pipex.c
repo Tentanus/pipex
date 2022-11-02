1044b0136a2eea0e3f680da9d4bd240e197558a3
Martijn Weverling <mweverli@student.codam.nl>
2022-11-02 21:44:41 +0100

    bump

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mweverli <mweverli@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/18 01:06:03 by mweverli      #+#    #+#                 */
-/*   Updated: 2022/10/31 00:04:08 by mweverli      ########   odam.nl         */
+/*   Updated: 2022/11/02 21:43:57 by mweverli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ int	main(int argc, char **argv, char **envp)
 {
 	int		pipefd[2];
 
-
+	if (argc != 5)
+		return ();
 }
